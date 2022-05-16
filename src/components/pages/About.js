2c5ca974122500afc4499e6e5c3d3458ca0a3da4
om praksash pattjoshi <omprakashpattjoshi123@gmail.com>
2022-05-16 22:38:33 +0530

    react-CRUD app

---
+++ src/components/pages/About.js
@@ -0,0 +1,33 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div className="container">
+      <div className="py-4">
+        <h1>About Page</h1>
+        <p className="lead">
+          Hey, Frands I am Om prakash. In this project i Learn React dinamic
+          routing, show Error page , CRUD application, Hooks, axios, ... Use can
+          Create, Update and Read and Delete application for his/her opction.
+          data fatch form axios. we have to start both server then it's work
+          1.yarn start 2. npm run json-server
+        </p>
+
+        <p className="lead">
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cumque rerum
+          hic ab veniam reiciendis cum repudiandae, voluptate explicabo nesciunt
+          nam accusantium? Soluta cupiditate, accusamus commodi praesentium
+          laborum dolorum libero maiores!
+        </p>
+        <p className="lead">
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cumque rerum
+          hic ab veniam reiciendis cum repudiandae, voluptate explicabo nesciunt
+          nam accusantium? Soluta cupiditate, accusamus commodi praesentium
+          laborum dolorum libero maiores!
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default About;
